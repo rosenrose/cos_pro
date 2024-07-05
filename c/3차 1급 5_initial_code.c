6a57059f3e6aaa25f6f283ddcbe2ca973b8b572d
JWK <jangwook1.kim@toshiba.co.jp>
2024-07-05 17:53:46 +0900

    sprintf

--- c/3차 1급 5_initial_code.c
+++ c/3차 1급 5_initial_code.c
@@ -3,42 +3,21 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#define DISPLAY_LEN 14
-
-char* substr(char* src, int idx, int len) {
-    char* dst = (char*)calloc(sizeof(char), len + 1);
-    strncpy(dst, src + idx, len);
-    dst[len] = 0;
-
-    return dst;
-}
 
 char* solution(char* phrases, int second) {
-    int second_passed = second % (DISPLAY_LEN * 2);
-    char* answer = (char*)calloc(sizeof(char), DISPLAY_LEN + 1);
+    char* placeholder = "______________";
+    int placeholder_len = strlen(placeholder);
+    int total_text_len = placeholder_len * 2 + strlen(phrases);
 
-    if (second_passed <= DISPLAY_LEN) {
-        int phrase_start_idx = DISPLAY_LEN - second_passed;
-        int visible_phrase_len = second_passed;
+    char* total_text = (char*)calloc(sizeof(char), total_text_len + 1);
+    char* answer = (char*)calloc(sizeof(char), placeholder_len + 1);
 
-        for (int i = 0; i < phrase_start_idx; i++) {
-            answer[i] = '_';
-        }
+    sprintf(total_text, "%s%s%s", placeholder, phrases, placeholder);
+    strncpy(answer, total_text + (second % 28), placeholder_len);
 
-        strncpy(answer + phrase_start_idx, phrases, visible_phrase_len);
-    } else {
-        int phrase_start_idx = second_passed - DISPLAY_LEN;
-        int visible_phrase_len = strlen(phrases) - phrase_start_idx;
+    total_text[total_text_len] = 0;
+    answer[placeholder_len] = 0;
 
-        strncpy(answer, phrases + phrase_start_idx, visible_phrase_len);
-
-        for (int i = visible_phrase_len; i < DISPLAY_LEN; i++) {
-            answer[i] = '_';
-        }
-    }
-
-    answer[DISPLAY_LEN] = 0;
-    
     return answer;
 }
 
